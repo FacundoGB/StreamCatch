de9adc7da6ce24152dcb7ddd1a27f64d99d8549b
FacundoGB <bardifacundo@gmail.com>
2021-11-08 16:09:06 -0300

    agrego query busqueda por email

--- src/main/java/com/StreamCatch/app/Repository/UserRepository.java
+++ src/main/java/com/StreamCatch/app/Repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.StreamCatch.app.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.StreamCatch.app.Entity.User;
+import com.StreamCatch.app.Entity.Users;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<Users, String> {
 
+	@Query("SELECT u FROM Users u WHERE u.email = :email")
+	public Users searchByEmail(@Param("email") String email);
 }
