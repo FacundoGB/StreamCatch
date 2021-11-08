428d31608cf793de1c76b3e8cdc10250a8297982
Dante Venini <veninidante@gmail.com>
2021-11-08 18:03:30 -0300

    Modificacion para imagen controlador de contenido 08/11 18 horas dante

--- src/main/java/com/StreamCatch/app/Controller/ContentController.java
+++ src/main/java/com/StreamCatch/app/Controller/ContentController.java
@@ -11,9 +11,41 @@ import com.StreamCatch.app.Service.ContentService;
 @RequestMapping("/content")
 public class ContentController {
 
-		@Autowired
-		private ContentService serieService;
-		private final String viewPath = "content/";
+	@Autowired
+	private ContentService contentService;
+	private final String viewPath = "content/";
+
+	@GetMapping()
+	public String contentPanel(ModelMap ContentModel) {
+		
+		
+		return null;
+	}
+		
+	
+	@GetMapping("/crear")
+	public String create() {
+
+		return this.viewPath.concat("crear-contenido");
+
+	}
+
+	@PostMapping("/create")
+	public String runCreate(ModelMap model, @RequestParam("name") String name) {
+
+		try {
+
+			contentService.createContent(name);
+			
+
+		} catch (Exception e) {
+			model.put("error", e.getMessage());
+			return "register";
+		}
+		
+		return "redirect:/".concat(viewPath);
+
+	}
 
 
 }
