472304f73f9f45d72d33b268562acad37afa8e94
Dante Venini <veninidante@gmail.com>
2021-11-08 18:22:40 -0300

    Modificacion atributo imagen dentro de la entidad contenido Dante 08/11 18:22 horas

--- src/main/java/com/StreamCatch/app/Entity/Content.java
+++ src/main/java/com/StreamCatch/app/Entity/Content.java
@@ -17,19 +17,31 @@ public class Content {
 	private String name;
 	@OneToOne
 	private Platform idPlatform;
-	
+	@Lob
+	@Column(columnDefinition="MEDIUMBLOB")
+	private String image;
+
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+
 	public String getId() {
 		return id;
 	}
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public Platform getIdPlatform() {
 		return idPlatform;
 	}
