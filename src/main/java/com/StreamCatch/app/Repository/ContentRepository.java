a849eeaa98b8626f5ca3e10dd7a70b6329104e46
Dante Venini <veninidante@gmail.com>
2021-11-09 20:26:41 -0300

    actualizacion de repositorio de contenido Dante 9/11 20:25 horas

--- src/main/java/com/StreamCatch/app/Repository/ContentRepository.java
+++ src/main/java/com/StreamCatch/app/Repository/ContentRepository.java
@@ -1,11 +1,17 @@
 package com.StreamCatch.app.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.StreamCatch.app.Entity.Content;
 
 @Repository
 public interface ContentRepository extends JpaRepository<Content, String>{
-	
+		
+	@Query("SELECT u from Content u WHERE u.id = :id")
+	public Optional<Content> findById(@Param("id") String id);
 }
