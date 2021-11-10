9921d8c65e19e89d80317152fdd1ab7714d5aab3
Dante Venini <veninidante@gmail.com>
2021-11-10 18:50:50 -0300

    solución servicio Dante 10/11 18:50 horas

--- src/main/java/com/StreamCatch/app/Service/ContentService.java
+++ src/main/java/com/StreamCatch/app/Service/ContentService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
@@ -16,7 +17,7 @@ import com.StreamCatch.app.Exceptions.ErrorException;
 import com.StreamCatch.app.Exceptions.ValidationError;
 import com.StreamCatch.app.Repository.ContentRepository;
 
-
+@Service
 public class ContentService{
 	
 	@Autowired
